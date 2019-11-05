6bb84229cb5a63a9790777d3bbe13375d9d03b4d
Daniel <danmoran.pro@gmail.com>
2019-11-05 13:36:02 -0700

    offset test and class test complete

--- lib/offset.rb
+++ lib/offset.rb
@@ -6,4 +6,19 @@ attr_reader :offset
     @offset = offset
   end
 
+  def squared
+  squared = (offset.to_i * offset.to_i).to_s
+  squared[-4..-1]
+end
+
+def hashed
+  offsets_arr = squared
+  {
+    a: offsets_arr[0].to_i,
+    b: offsets_arr[1].to_i,
+    c: offsets_arr[2].to_i,
+    d: offsets_arr[3].to_i
+  }
+end
+
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -6,42 +6,43 @@ class OffsetTest < Minitest::Test
   def setup
     @offset_1 = Offset.new
     @offset_2 = Offset.new("051119")
+    @offset_3 = Offset.new("021093")
   end
-
-  def test_offset_exists
-    assert_instance_of Offset, @offset_2
-  end
-
-  # def test_can_take_date_or_use_current_date
-  #   @offset_1.date.stubs(:date).returns("021093")
   #
-  #   assert_equal "140919", @offset_2.date
+  # def test_offset_exists
+  #   assert_instance_of Offset, @offset_2
+  # end
+  #
+  # def test_can_take_date_or_use_current_date
+  #   @offset_1.offset.stubs(:offset).returns("021093")
+  #
+  #   assert_equal "051119", @offset_2.offset
   # end
   #
   # def test_date_is_squared_and_returns_last_4_numbers
-  #   @offset_1.date.stubs(:keys).returns("021093")
-  #   assert_equal "4561",@offset_2.squared
+  #   @offset_1.offset.stubs(:key).returns("021093")
+  #   assert_equal "2161",@offset_2.squared
   # end
-  #
-  # def test_offset_array_can_be_converted
-  #   expected = {
-  #     :a=>4,
-  #     :b=>5,
-  #     :c=>6,
-  #     :d=>1
-  #   }
-  #
-  #   assert_equal expected, @offset_1.hashed
-  #
-  #   expected = {
-  #     :a=>4,
-  #     :b=>5,
-  #     :c=>6,
-  #     :d=>1
-  #   }
-  #
-  #   assert_equal expected, @offset_2.hashed
-  # end
+
+  def test_offset_array_can_be_converted
+    expected = {
+      :a=>4,
+      :b=>6,
+      :c=>4,
+      :d=>9
+    }
+
+    assert_equal expected, @offset_3.hashed
+
+    expected = {
+      :a=>2,
+      :b=>1,
+      :c=>6,
+      :d=>1
+    }
+
+    assert_equal expected, @offset_2.hashed
+  end
 
 
 end
